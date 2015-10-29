c4c03a673cb2f34fee387d134bcd9e2754401993
jsfumato <jsfumato@naver.com>
2015-10-29 21:02:01 +0900

    [2-B] PlayerData 객체구현. food 관련 메소드 및 캐릭터리스트 보유

--- Cocos_practice/Classes/PlayerData.h
+++ Cocos_practice/Classes/PlayerData.h
@@ -5,6 +5,22 @@
 //멤버변수들로 데이터를 들고 있고 겟으로 받아온다
 class PlayerData
 {
+public:
+	PlayerData();
+	~PlayerData();
 
+	int		getFood();
+	void	setFood(int _food);
+	void	addFood(int num);
+
+	std::list<Character*> getCharacterList();
+	void	addCharacter(Character* character);
+	void	removeCharacter(Character* character);
+	
+	PlayerData* opponentPlayer;
+
+private:
+	int		food;
+	std::list<Character*> characterList;
 };
 #endif // PlayerData_h__
