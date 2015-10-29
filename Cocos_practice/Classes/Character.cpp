788a46a862413ebb814e7d162750cbcd906ceaba
atgchan <atgchan@gmail.com>
2015-10-29 23:15:33 +0900

    character class update
    character class update

--- Cocos_practice/Classes/Character.cpp
+++ Cocos_practice/Classes/Character.cpp
@@ -3,17 +3,79 @@
 #include "Character.h"
 
 
-Character* Character::create(PlayerInfo currentPlayerInfo)
+Character* Character::create(PlayerInfo cPInfo)
 {
 	Character* sprite = new Character();
+	std::string fileName = std::string("Character/spear_");
 
-	if (sprite && (sprite->initWithFile(파일이름)))
+	if (cPInfo == PLAYER_RED)
+	{
+		fileName += std::string("red");
+	}
+	else if (cPInfo == PLAYER_BLUE)
+	{
+		fileName += std::string("blue");
+	}
+	fileName += std::string("_00.png");
+
+	if (sprite && (sprite->initWithFile(fileName)))
 	{
 		sprite->autorelease();
-		sprite->setOwnerCharacter(currentPlayerInfo);
+		sprite->setcurrentPlayerInfo(cPInfo);
+		sprite->setcurrentAngle(ANGLE_DOWN_LEFT);
 		//기타 필요한거
 		return sprite;
 	}
 	CC_SAFE_DELETE(sprite);
 	return nullptr;
+}
+
+bool Character::isOnTile(TileKind tileTypeToCheck)
+{
+	if (currentTile->getTypeOfTile() == tileTypeToCheck)
+	{
+		return true;
+	}
+	else return false;
+}
+
+void Character::rotateToDirection(RotateDirection rotateDirection, Character* character)
+{
+	std::string fileName = std::string("Character/spear_");
+	if (character->getcurrentPlayerInfo() == PLAYER_RED)
+	{
+		fileName += std::string("red");
+	}
+	else if (character->getcurrentPlayerInfo() == PLAYER_BLUE)
+	{
+		fileName += std::string("blue");
+	}
+
+	int cAngle = character->getcurrentAngle();
+	if (rotateDirection == ROTATE_LEFT)
+	{
+		if (cAngle == 5) cAngle = 0;
+		else cAngle++;
+
+		std::ostringstream ostr;
+		ostr << cAngle;
+		fileName += std::string("_0");
+		fileName += std::string(ostr.str());
+		fileName += std::string(".png");
+	}
+	else if (rotateDirection == ROTATE_RIGHT)
+	{
+		if (cAngle == 0) cAngle = 5;
+		else cAngle--;
+
+		std::ostringstream ostr;
+		ostr << cAngle;
+		fileName += std::string("_0");
+		fileName += std::string(ostr.str());
+		fileName += std::string(".png");
+	}
+	character->setcurrentAngle(CharacterAngle(cAngle));
+
+	character->initWithFile(fileName);
+	character->setAnchorPoint(Point(0.5, 0.13));
 }
--- Cocos_practice/Classes/Character.h
+++ Cocos_practice/Classes/Character.h
@@ -1,6 +1,8 @@
 #ifndef Character_h__
 #define Character_h__
 
+#include "Self_Tile.h"
+
 
 enum CharacterAngle{
 	ANGLE_DOWN_LEFT,
@@ -12,16 +14,39 @@ enum CharacterAngle{
 	ANGLE_ERR = -1
 };
 
+enum RotateDirection
+{
+	ROTATE_LEFT,
+	ROTATE_RIGHT,
+	ROTATE_ERR = -1
+};
+
 
 class Character : public cocos2d::Sprite
 {
 public:
+	/*
+	Autor: Wonseok
+	Description: 플레이어의 종류(레드, 블루)를 받아와서 해당 팀에 속하는 색깔의 캐릭터 스프라이트를 생성한다.
+	*/
 	static Character* create(PlayerInfo currentPlayerInfo);
-	void setRotate();
-	bool isOnTile(TileKind);
+
+	/*
+	Author: Wonseok
+	Description: 방향을 받아서 해당 방향으로 스프라이트 이미지를 회전한다.
+	*/
+	void rotateToDirection(RotateDirection, Character* character);
+
+	/*
+	Autor: Wonseok
+	Description: 타일 카인드를 하나 받아와서 현재 캐릭터가 올라와있는 타일과 동일한 종류인지 검사하고 bool을 리턴한다.
+	*/
+	bool isOnTile(TileKind tileTypeToCheck);
+
 private:
-	//getter, setter neeeeeeed
-	CharacterAngle currentAngle;
+	CC_SYNTHESIZE(CharacterAngle, currentAngle, currentAngle);
+	CC_SYNTHESIZE(Self_Tile*, currentTile, currentTile);
+	CC_SYNTHESIZE(PlayerInfo, currentPlayerInfo, currentPlayerInfo);
 
 };
 #endif // Character_h__
