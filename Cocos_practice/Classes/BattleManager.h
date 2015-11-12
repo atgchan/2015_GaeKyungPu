01a146ac2ad9e2bf45961ded41c71b4625a39b77
Sorae <seokmin@nhnnext.org>
2015-11-13 01:00:59 +0900

    배틀매니저 틀
    틀만들기

--- Cocos_practice/Classes/BattleManager.h
+++ Cocos_practice/Classes/BattleManager.h
@@ -9,6 +9,10 @@ public:
 	BattleManager();
 	~BattleManager();
 	void	BattleBetween(Character* attacker, Character* defender);
-	void	GetAttackFormation(Character* attacker);
-	void	getDefenseFormation(Character* defender);
+	void	SetAttackFormation(Character* attacker);
+	void	SetDefenseFormation(Character* defender);
+
+private:
+	std::list<Character*>	_CurrentAttackFormation;
+	std::list<Character*>	_CurrentDefenseFormation;
 };
