387a3d486b9b101a1aadd66d4c7bdf175f6300fa
atgchan <atgchan@gmail.com>
2015-11-22 09:45:46 +0900

    볼케이노 임시 문자 출력

--- Cocos_practice/Classes/Phase_Volcano.cpp
+++ Cocos_practice/Classes/Phase_Volcano.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 #include "Phase_Volcano.h"
+#include "TileMap.h"
+#include <cstdlib>
+#include <ctime>
 
 
 Phase_Volcano::Phase_Volcano()
@@ -13,18 +16,30 @@ Phase_Volcano::~Phase_Volcano()
 
 void Phase_Volcano::Tick()
 {
+	int randNum;
+	if (GM->getIsVolcanoActivated())
+	{
+		GM->setVolcanoActivated(false);
+		GM->setProgressVolcano(0);
+	}
 	switch (GM->getProgressVolcano())
 	{
-	case 0:
-		if (cocos2d::random(1, 5) == 1)//5분의 1 확률로 이벤트 발생
+	case 0: 
+		std::srand(std::time(0));
+		randNum = (std::rand() % 5);
+		if (randNum == 1)//5분의 1 확률로 이벤트 발생
 		{
 			GM->setVolcanoActivated(true);
 			GM->setProgressVolcano(1);
+			cocos2d::Label* explositionMent = cocos2d::Label::createWithTTF("Fire in the hole!!!!!", "fonts/Marker Felt.ttf", 20);
+			explositionMent->setPosition(cocos2d::Vec2(200, 300));
+			TileMap::getInstance()->addChild(explositionMent);
 		}
 		break;
 
 	default:
 		break;
 	}
+
 	_NextPhase = PHASE_ACTION;
 }
