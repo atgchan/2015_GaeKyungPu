873573fbea9b42583bf8e8799e92279f1ca3dbf1
Sorae <seokmin@nhnnext.org>
2015-11-23 19:18:55 +0900

    주석삭제
    주석삭제

--- Cocos_practice/Classes/HistoryEventKillCharacter.cpp
+++ Cocos_practice/Classes/HistoryEventKillCharacter.cpp
@@ -24,19 +24,7 @@ HistoryEventKillCharacter::~HistoryEventKillCharacter()
 
 void HistoryEventKillCharacter::Run()
 {	
-	/*auto removeCall = CallFunc::create(CC_CALLBACK_0(TileMap::removeChild, TileMap::getInstance(), _CharacterToKill.get(), true));
-	auto nextCall = CallFunc::create(CC_CALLBACK_0(AnimationManager::PlayHistory, AnimationManager::getInstance()));
-	
-
-	FiniteTimeAction* seq = Sequence::create(removeCall,nextCall, nullptr);
-	//seq->startWithTarget(AnimationManager::getInstance()->getNode());
-	
-	//_CharacterToKill->runAction(seq);
-	AnimationManager::getInstance()->getNode()->runAction(seq);
-*/
-
 	TileMap::getInstance()->removeChild(_CharacterToKill.get());
-	//AnimationManager::getInstance()->PlayHistory();
 	_Done = true;
 	CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("Sound/Hit_03.wav");
 }
