eacc49a526f1af061f612a0a2cc656efaf4b54e6
Sorae <seokmin@nhnnext.org>
2015-12-15 00:24:01 +0900

    dicedice display 삭제
    와꾸는 놔둠

--- Cocos_practice/Classes/DiceDice.cpp
+++ Cocos_practice/Classes/DiceDice.cpp
@@ -18,20 +18,7 @@ int DiceDice::RollDiceBetween(int start, int end)
 
 void DiceDice::DisplayDiceOnScreen(int num)
 {
-	cocos2d::Label* numToDisplay = cocos2d::Label::createWithTTF(std::to_string(num), "fonts/Marker Felt.ttf", 20);
-	//numToDisplay->enableOutline(Color4B::WHITE, 1);
-
-	float height = TileMap::getInstance()->getContentSize().height;
-	float width = TileMap::getInstance()->getContentSize().width;
-
-	numToDisplay->setPosition(cocos2d::Vec2(200, 200));
-
-	TileMap::getInstance()->addChild(numToDisplay);
-
-	auto delay = DelayTime::create(2);
-	auto removeFunc = CallFunc::create(CC_CALLBACK_0(DiceDice::RemoveDiceFromScreen, this, numToDisplay));
-	auto seq = Sequence::create(delay, removeFunc, NULL);
-	numToDisplay->runAction(seq);
+	//추후추가
 }
 
 void DiceDice::RemoveDiceFromScreen(cocos2d::Label* numToDisplay)
